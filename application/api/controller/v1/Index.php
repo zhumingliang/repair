e80be3262344c44920d5cfb413a3879c02f2c107
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-10-22 10:54:48 +0800

    项目初始化

--- application/api/controller/v1/Index.php
+++ application/api/controller/v1/Index.php
@@ -6,7 +6,9 @@ class Index
 {
     public function index()
     {
-        echo 'success';
+        $token = "4013e96782dcf82dcf8bc0d5cd51b202";
+        $info = \app\api\service\Token::getCurrentTokenVar();
+        return json($info);
     }
 
     public function hello($name = 'ThinkPHP5')
