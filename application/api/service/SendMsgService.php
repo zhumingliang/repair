f5f09bba91dc9deb6e4d480b482fd9caa461dd04
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-12-06 16:09:49 +0800

    项目初始化

--- application/api/service/SendMsgService.php
+++ application/api/service/SendMsgService.php
@@ -82,6 +82,7 @@ class SendMsgService
 
     private function sendSms($type)
     {
+        LogT::create(['msg'=>$this->phone]);
         SendSms::instance()->send($this->phone, $this->params, $type);
 
     }
